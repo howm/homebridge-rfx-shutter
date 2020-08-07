6c31b8393798df4922847cc174062a00e761522c
momsse <momssesystem@gmail.com>
2020-08-07 11:52:44 +0200

    :sparkles: platform model

--- README.md
+++ README.md
@@ -4,6 +4,12 @@
 
 > Homebridge plugin for shutters controlled by RFXtrx433(E) transceivers.
 
+### Warning
+
+`v1.x.x` version of this plugin have issues with latest `rfxcom` versions (at least > `2.0.2`). The multiple accessories model fire multiple `listRemotes` (one for each accessory defined on your config file) that overwhelm the transceivers. `v2.x.x` use platform model that prevent this issue.
+
+@see [V1 Readme file](V1_README.md) for `v1.x.x` usage.
+
 ### Install
 
 ```bash
@@ -12,35 +18,23 @@ npm i -g homebridge-rfx-shutter
 
 ### Usage
 
-In your `~/.homebridge/config.json` on the accessories part add your shutter using the following example:
+In your `~/.homebridge/config.json` on the `platform` part add your shutter using the following example:
 
 ```json
     {
-      "accessory": "HomebridgeRfxShutter",
+      "platform": "RFXShutterDynamicPlatform",
       "tty": "/dev/ttyUSB0",
-      "name": "Shutter 1",
-      "deviceId": "0x0FFFFF/1",
       "openSeconds": 20,
-      "closeSeconds": 20
+      "closeSeconds": 20,
+      "excludedDeviceIds": ["0x0D7337/1"]
     }
 ```
 
-Where `tty` is the device "teletype" that can be found using the following command on linux systems:
-
-```bash
-system_profiler SPUSBDataType
-```
-
-Shutters device ids are logged at the plugin startup, eg:
-
-```text
-[2019-8-19 22:41:19] [Shutter 1] Found device 0x0FFFFA/1
-[2019-8-19 22:41:19] [Shutter 1] Found device 0x0FFFFB/1
-[2019-8-19 22:41:19] [Shutter 1] Found device 0x0FFFFC/1
-[2019-8-19 22:41:19] [Shutter 1] Found device 0x0FFFFD/1
-```
-
-Some shutters close slower or quicker than their opening, we can then define an optional custom `closeSeconds` for them. This value is default to `openSeconds`.
+- `tty` is the device "teletype" that can be found using `find-rfxcom` [rfxcom utility script](https://github.com/rfxcom/node-rfxcom#utility-scripts).
+- `openSeconds` / `closeSeconds` are optionals, by default they are set to `25` for opening and `22` seconds for closing.
+- `excludedDeviceIds` option can be used to remove any shutter or "shutter group" to the platform. The device id of the shutter is provided in the accessory information, see below:
+![Accessory Information](images/accessory-information.png)
+![Accessory Information](images/accessory-detected.png)
 
 ### Release
 
---
+++ src/accessory/ShutterAccessory.ts
@@ -0,0 +1,166 @@
+import {
+  API,
+  CharacteristicEventTypes,
+  CharacteristicGetCallback,
+  CharacteristicSetCallback,
+  HAP,
+  Logging,
+  PlatformAccessory,
+  PlatformAccessoryEvent,
+} from 'homebridge';
+import wait from 'waait';
+import { ShutterAction, fireShutterAction } from '../service/rfxcom';
+
+const DEFAULT_OPEN_SECONDS = 25;
+const DEFAULT_CLOSE_SECONDS = 22;
+enum PositionState {
+  DECREASING = 0,
+  INCREASING = 1,
+  STOPPED = 2,
+}
+
+export interface ShutterAccessoryConfig {
+  openSeconds?: number;
+  closeSeconds?: number;
+}
+
+export default class ShutterAccessory {
+  private readonly log: Logging;
+
+  private readonly hap: HAP;
+
+  public readonly accessory: PlatformAccessory;
+
+  private currentPosition: number = 0;
+
+  private targetPosition: number = 0;
+
+  private readonly deviceId: string;
+
+  private positionState: number = PositionState.STOPPED;
+
+  private readonly config: ShutterAccessoryConfig;
+
+  public constructor(
+    log: Logging,
+    api: API,
+    accessory: PlatformAccessory,
+    config: ShutterAccessoryConfig,
+  ) {
+    this.log = log;
+    this.hap = api.hap;
+    this.accessory = accessory;
+    this.deviceId = accessory.context.deviceId;
+    this.config = config;
+
+    accessory.on(PlatformAccessoryEvent.IDENTIFY, (): void => {
+      this.log(accessory.displayName, ' identified!');
+    });
+
+    // AccessoryInformation
+    const accessoryInformationService = accessory.getService(
+      this.hap.Service.AccessoryInformation,
+    );
+    accessoryInformationService?.setCharacteristic(
+      this.hap.Characteristic.Manufacturer,
+      'RFX',
+    );
+    accessoryInformationService?.setCharacteristic(
+      this.hap.Characteristic.Model,
+      'RFXtrx433E',
+    );
+    accessoryInformationService?.setCharacteristic(
+      this.hap.Characteristic.SerialNumber,
+      this.deviceId,
+    );
+
+    // WindowCovering
+    const windowCoveringService = this.accessory.getService(
+      this.hap.Service.WindowCovering,
+    )!;
+    windowCoveringService
+      .getCharacteristic(this.hap.Characteristic.CurrentPosition)
+      .on(CharacteristicEventTypes.GET, this.getCurrentPosition.bind(this));
+
+    windowCoveringService
+      .getCharacteristic(this.hap.Characteristic.TargetPosition)
+      .on(CharacteristicEventTypes.GET, this.getTargetPosition.bind(this))
+      .on(CharacteristicEventTypes.SET, this.setTargetPosition.bind(this));
+
+    windowCoveringService
+      .getCharacteristic(this.hap.Characteristic.PositionState)
+      .on(CharacteristicEventTypes.GET, this.getPositionState.bind(this));
+  }
+
+  public getCurrentPosition(cb: CharacteristicGetCallback<number>): void {
+    this.log(`getCurrentPosition ${this.currentPosition} for ${this.deviceId}`);
+    cb(null, this.currentPosition);
+  }
+
+  public getTargetPosition(cb: CharacteristicGetCallback<number>): void {
+    this.log(`getTargetPosition ${this.targetPosition} for ${this.deviceId}`);
+    cb(null, this.targetPosition);
+  }
+
+  public async setTargetPosition(
+    value: number,
+    cb: CharacteristicSetCallback,
+  ): Promise<void> {
+    this.log(`setTargetPosition ${value} for ${this.deviceId}`);
+    /**
+     * It seems that the cb need to be called to say that we understand the action. When delayed
+     * until the position stop Siri complain about communication problem with the device.
+     */
+    cb(null, value);
+
+    this.targetPosition = value;
+
+    if (this.targetPosition === this.currentPosition) return;
+
+    const up: boolean = this.targetPosition > this.currentPosition;
+    this.setPositionState(
+      up ? PositionState.INCREASING : PositionState.DECREASING,
+    );
+    fireShutterAction(
+      this.deviceId,
+      up ? ShutterAction.UP : ShutterAction.DOWN,
+    );
+
+    await wait(
+      (Math.abs(this.targetPosition - this.currentPosition) / 100) *
+        ((up
+          ? this.config.openSeconds || DEFAULT_OPEN_SECONDS
+          : this.config.closeSeconds || DEFAULT_CLOSE_SECONDS) *
+          1000),
+    );
+
+    if (this.targetPosition !== 100 && this.targetPosition !== 0) {
+      // Otherwise the stop button will fire the "memo" command
+      this.log(`fireShutterAction ${ShutterAction.STOP}`);
+      fireShutterAction(this.deviceId, ShutterAction.STOP);
+    }
+    this.setPositionState(PositionState.STOPPED);
+    this.setCurrentPosition(this.targetPosition);
+  }
+
+  public setCurrentPosition(value: number): void {
+    this.log(`setCurrentPosition to ${value} for ${this.deviceId}`);
+    this.currentPosition = value;
+    this.accessory
+      .getService(this.hap.Service.WindowCovering)!
+      .setCharacteristic(this.hap.Characteristic.CurrentPosition, value);
+  }
+
+  public getPositionState(cb: CharacteristicGetCallback<PositionState>): void {
+    this.log(`getPositionState ${this.positionState} for ${this.deviceId}`);
+    cb(null, this.positionState);
+  }
+
+  public setPositionState(positionState: PositionState): void {
+    this.log(`setPositionState to ${positionState} for ${this.deviceId}`);
+    this.positionState = positionState;
+    this.accessory
+      .getService(this.hap.Service.WindowCovering)!
+      .setCharacteristic(this.hap.Characteristic.PositionState, positionState);
+  }
+}
