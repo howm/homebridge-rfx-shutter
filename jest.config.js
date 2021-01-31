cf9d80385fd02171c3434505b598265a5382b56a
momsse <1344228+momsse@users.noreply.github.com>
2021-01-31 14:55:05 +0100

    :heavy_minus_sign: remove jest config

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  moduleFileExtensions: ['ts', 'js', 'json'],
-  transform: {
-    '^.+\\.ts$': 'ts-jest',
-  },
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  testPathIgnorePatterns: ['dist'],
-};
