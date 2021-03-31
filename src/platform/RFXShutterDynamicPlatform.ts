3be35693669a3f16f7123eebe52f909003dc2bce
momsse <1344228+momsse@users.noreply.github.com>
2021-03-31 20:26:46 +0200

    :bug: fix missing direction on accessory initialization

--- src/platform/RFXShutterDynamicPlatform.ts
+++ src/platform/RFXShutterDynamicPlatform.ts
@@ -120,6 +120,7 @@ export default class RFXShutterDynamicPlatform
       new ShutterAccessory(this.log, this.api, accessory, {
         openSeconds: this.config.openSeconds,
         closeSeconds: this.config.closeSeconds,
+        direction: this.config.direction,
       }),
     );
   }
