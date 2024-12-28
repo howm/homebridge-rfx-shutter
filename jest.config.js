8d1aa9b9e39b402b296d65ada8f0bd2e14526cce
JurgenLB <77586573+JurgenLB@users.noreply.github.com>
2024-12-28 15:27:30 +0100

    Create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '^homebridge-rfx-shutter(.*)$': '<rootDir>/src$1',
+  },
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+};
